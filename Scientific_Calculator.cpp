20a795b948cd58e2cca964749baa17f016901829
Dhias Muhammad Naufal <dhiasmnaufal0@mail.ugm.ac.id>
2022-05-27 10:14:14 +0700

    Edit Tampilan Terminal

--- Scientific_Calculator.cpp
+++ Scientific_Calculator.cpp
@@ -1,26 +1,34 @@
 #include<iostream>
 #include<math.h> 
-#include "operasi_dasar.hpp"
-#include "trigonometri.hpp"
+
+//Include file operasi
+//#include "operasi_dasar.hpp"
+//#include "trigonometri.hpp"
+
 using namespace std;
 #define PI 3.14
 
 int main(){
-    operasi_dasar operasi;
-    int i;
+//Membuat Objek dari kelas
+//operasi_dasar operasi;
+
     float a,b ;
-    cout<<"******************** Scientific Calculator ******************\n"; 
-    cout<<"-------------------------------------------------------------\n"; 
-    cout<<"\t\tPilih operasi yang akan dilakukan:\t\t\t\t\n"; 
-    cout<<"-------------------------------------------------------------\n"; 
-    cout<<"1: Pertambahan\t\t"<<"7: Sin\t\t"<<"\t\t13: Log"<<endl; 
-    cout<<"2: Pengurangan\t\t"<<"8: Cos\t\t"<<"\t\t14: Log dengan basis 10"<<endl; 
+
+// Tampilan pada Terminal
+    cout<<"************************************ Scientific Calculator ********************************\n"; 
+    cout<<"-------------------------------------------------------------------------------------------\n"; 
+    cout<<"\t\t\t\tPilih operasi yang akan dilakukan:\t\t\t\t\n"; 
+    cout<<"-------------------------------------------------------------------------------------------\n"; 
+    cout<<"1: Pertambahan\t\t\t"<<"7: Sin\t\t"<<"\t\t13: Log"<<endl; 
+    cout<<"2: Pengurangan\t\t\t"<<"8: Cos\t\t"<<"\t\t14: Log dengan basis 10"<<endl; 
     cout<<"3: Perkalian\t\t\t"<<"9: Tan\t\t"<<endl; 
     cout<<"4: Pembagian\t\t\t"<<"10: Inverse dari Sin"<<endl; 
     cout<<"5: Exponen\t\t\t"<<"11: Inverse dari Cos"<<endl; 
     cout<<"6: Akar Kuadrat\t\t\t"<<"12: Inverse dari Tan"<<endl;
-    pilihan :
-    cout<<"Enter the function that you want to perform : ";
+
+// Switch Case Pilihan operasi yang ingin dijalankan
+    int i;
+    cout<<"Pilih Operasi yang ingin dilakukan : ";
     cin>>i;
     switch(i)
     { 
@@ -53,7 +61,6 @@ int main(){
         case 14: 
         break; 
 default: 
-cout<<"Inputnya salah, silahkan pilih lagi :D"<<endl;
-goto pilihan;
+cout<<"Inputnya salah, silahkan coba lagi :D"<<endl;
 } 
 }
